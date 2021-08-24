b3a262cbd0df285c1cbbeb6cf8c56305c9e09255
Ryan Patmore <rh923364@racc-login-0-2.local>
2021-08-24 16:50:06 +0100

    modified:   Plot/plot_time_series.py

--- Plot/plot_time_series.py
+++ Plot/plot_time_series.py
@@ -16,8 +16,8 @@ class time_series(object):
             print (self.path + case)
             mean = xr.open_mfdataset(self.path + case + 
                                '/Stats/SOCHIC_PATCH_mean*.nc',
-                               compat='override', coords='different',
-                               chunks={'time_counter':10})
+                               compat='override', coords='different')
+                               #chunks={'time_counter':10})
             std = xr.open_mfdataset(self.path + case + 
                                '/Stats/SOCHIC_PATCH_std*.nc',
                                compat='override', coords='different',
@@ -54,18 +54,61 @@ class time_series(object):
 
         ax.plot(self.ds.time_counter, self.ds[var + '_mean'])
 
-    def plot_mld_sip(self, year, giddy=False, orca=False, satellite=False):
+    def plot_heat_fluxes(self, year):
+        ''' plot heat fluxes into ocean'''
+
+        self.fig, self.axs = plt.subplots(3, 1, figsize=(5.5,4.0))
+        plt.subplots_adjust(left=0.16)
+
+        colours = ['royalblue', 'orange', 'olivedrab']
+        text_pos = [0.05, 0.2, 0.35]
+
+        for (case, data) in self.cases.items():
+            for i, (year, ds) in enumerate(data.groupby('time_counter.year')): 
+                colour = colours[i]
+                self.axs[0].text(text_pos[i], 1.05, year, c=colour,
+                            transform=self.axs[0].transAxes)
+
+                qt_oce = ds.qt_oce_mean.rolling(time_counter=7, center=True).mean()
+                self.axs[0].plot(qt_oce.time_counter.dt.dayofyear, qt_oce,
+                                 lw=1, c=colour)
+                qns_oce = ds.qns_oce_mean.rolling(time_counter=7, center=True).mean()
+                self.axs[1].plot(qns_oce.time_counter.dt.dayofyear, qns_oce,
+                                 lw=1, c=colour)
+                qsr_oce = ds.qsr_oce_mean.rolling(time_counter=7, center=True).mean()
+                self.axs[2].plot(qsr_oce.time_counter.dt.dayofyear, qsr_oce,
+                                 lw=1, c=colour)
+        for ax in self.axs:
+        #    #ax.set_xlim([self.ds.time_counter.min(),
+        #    #            self.ds.time_counter.max()])
+            ax.set_xlim(0,366)
+            ax.axhline(0, lw=0.5, c='black', ls=':', zorder=1)
+            #major_format = mdates.DateFormatter('%b')
+            #ax.xaxis.set_major_formatter(major_format)
+        for ax in self.axs[:-1]:
+            ax.set_xticks([])
+
+        self.axs[0].set_ylabel('surface\n heat flux')
+        self.axs[1].set_ylabel('non-solar\n heat flux')
+        self.axs[2].set_ylabel('solar\n heat flux')
+
+        self.axs[2].set_xlabel('day of year')
+
+        plt.savefig('heat_fluxes_EXP04.png', dpi=600)
+
+
+    def plot_mld_sip(self, years, giddy=False, orca=False, satellite=False):
 
         # alias case
 
         self.fig, self.axs = plt.subplots(2,1, figsize=(5.5,4.0))
 
-        colours = ['royalblue', 'orange', 'black']
-        text_pos = [0.05, 0.2]
+        colours = ['royalblue', 'orange', 'olivedrab']
+        text_pos = [0.05, 0.2, 0.35]
 
-        
+         
         for (case, data) in self.cases.items():
-            #data = data.sel(time_counter='2012')
+            #data = data.sel(time_counter=years[0])
             for i, (year, ds) in enumerate(data.groupby('time_counter.year')): 
                 colour = colours[i]
                 self.axs[0].text(text_pos[i], 1.05, year, c=colour,
@@ -78,17 +121,20 @@ class time_series(object):
 
                 # plot mixed layer depth
                 self.axs[0].fill_between(ds.time_counter.dt.dayofyear, 
-                                    lower, upper, alpha=0.2)
+                                    lower, upper, alpha=0.2, color=colour,
+                                    ec=None)
                 print ('plt1')
                 self.axs[0].plot(ds.time_counter.dt.dayofyear, ds.mldr10_3_mean, lw=1, 
                             c=colour)
                 print ('plt2')
 
                 print ('siconc')
-                self.axs[1].plot(ds.time_counter.dt.dayofyear, 
+                l, = self.axs[1].plot(ds.time_counter.dt.dayofyear, 
                             ds.icepres_mean, c=colour, ls='-', lw=1)
                 #print ('plt3')
 
+        lines = [l]
+        labels= ['SOCHIC ORCA12']
 
         for ax in self.axs:
         #    #ax.set_xlim([self.ds.time_counter.min(),
@@ -99,6 +145,7 @@ class time_series(object):
         for ax in self.axs[:-1]:
             ax.set_xticks([])
 
+        self.axs[0].set_ylim(-10,160)
         self.axs[0].set_ylabel('mixed layer depth')
         self.axs[1].set_ylabel('mean sea ice presence')
 
@@ -108,11 +155,15 @@ class time_series(object):
         if giddy:
             self.plot_mld_sip_add_giddy()
         if orca:
-            self.plot_mld_sip_add_orca()
+            lines.append(self.plot_mld_sip_add_orca(colours, years))
+            labels.append('global ORCA12')
         if satellite:
-            self.plot_mld_sip_add_satellite(colours)
+            lines.append(self.plot_mld_sip_add_satellite(colours, years))
+            labels.append('satellite obs.')
+        
+        self.axs[0].legend(lines, labels, fontsize=8)
 
-        plt.savefig('mld_siconc_EXP04.png', dpi=600)
+        plt.savefig('mld_siconc_EXP04_all_with_giddy.png', dpi=600)
 
     def plot_mld_sip_add_giddy(self):
         ''' add giddy to mix layer depth '''
@@ -125,30 +176,39 @@ class time_series(object):
         self.axs[0].plot(self.giddy.dayofyear, self.giddy.mld, lw=1, 
                             c='red')
 
-    def plot_mld_sip_add_orca(self):
+    def plot_mld_sip_add_orca(self, colours, year):
         ''' add orca to sea ice presence '''
 
         self.orca = xr.open_dataset(self.root + 
-                    'NemoOut/ORCA/ORCA_PATCH_mean_I.nc')
+                    'NemoOut/ORCA/Stats/ORCA_PATCH_mean_I.nc')
         
-        self.orca = self.orca.assign_coords({'dayofyear':
-                                      self.orca.time_counter.dt.dayofyear})
-        self.orca = self.orca.swap_dims({'time_counter':'dayofyear'})
-        self.axs[1].plot(self.orca.dayofyear, self.orca.siconc_mean, lw=1, 
-                            c='green')
+        #self.orca = self.orca.assign_coords({'dayofyear':
+        #                              self.orca.time_counter.dt.dayofyear})
+        #self.orca = self.orca.swap_dims({'time_counter':'dayofyear'})
 
-    def plot_mld_sip_add_satellite(self, colours):
+        #print (year)
+        #self.orca = self.orca.sel(time_counter=year[0])
+
+        for i, (year, ds) in enumerate(self.orca.groupby('time_counter.year')): 
+            colour = colours[i]
+            l, = self.axs[1].plot(ds.time_counter.dt.dayofyear, ds.siconc_mean, lw=1, 
+                                c=colour, ls=':')
+        return l
+
+    def plot_mld_sip_add_satellite(self, colours, year):
         ''' add satellite to sea ice presence '''
 
         print (self.root + 'NemoOut/SeaIce/seaice_conc_daily_sh_*mean.nc')
         self.sat = xr.open_mfdataset(self.root + 
                     'NemoOut/SeaIce/seaice_conc_daily_sh_*mean.nc')
         
+        #self.sat = self.sat.sel(time=year[0])
         for i, (year, ds) in enumerate(self.sat.groupby('time.year')): 
-            if year == 2014: continue
+            #if year == 2014: continue
             colour = colours[i]
-            self.axs[1].plot(ds.time.dt.dayofyear, 
+            l, =  self.axs[1].plot(ds.time.dt.dayofyear, 
                              ds.icepres_mean, c=colour, ls='--', lw=1)
+        return l
 
 
 
@@ -229,5 +289,7 @@ class time_series(object):
 
 
 ds = time_series(['EXP04'])
-ds.plot_mld_sip(['2012', '2013'], giddy=True, orca=True, satellite=True)
+ds.plot_mld_sip(['2014'], giddy=True, orca=True, satellite=True)
+#ds.plot_mld_sip(['2012', '2013', '2014'], giddy=True, orca=True, satellite=True)
+#ds.plot_heat_fluxes(['2012', '2013', '2014'])
 #ds.add_orca12_sip()
