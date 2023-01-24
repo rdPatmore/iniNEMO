58e8af1eaf9b90bd8e7ebbf0c74aaa5b8568e993
Ryan Patmore <r.d.patmore@reading.ac.uk>
2023-01-24 16:28:25 +0000

    refined plotting

--- Plot/plot_momentum.py
+++ Plot/plot_momentum.py
@@ -17,7 +17,8 @@ class plot_momentum(object):
         ''' plot budget of KE '''
         
         # ini figure
-        fig, axs = plt.subplots(4, 4, figsize=(5.5,5.5))
+        fig, axs = plt.subplots(2, 4, figsize=(5.5,5.5))
+        plt.subplots_adjust(right=0.78, hspace=0.25, wspace=0.1, top=0.9)
 
         # load and slice
         ds = xr.open_dataset(self.preamble + 'mom' + vec + '.nc')
@@ -26,68 +27,59 @@ class plot_momentum(object):
         ds = ds.isel(time_counter=1).load()            # time
 
             
-        if vec == 'u':
-            missing = 'trd_udx'
-        if vec == 'v':
-            missing = 'trd_vdy'
-        var_list = ['trd_hpg', 'trd_spg', 'trd_spgexp', 'trd_spgflt',
-                    'trd_keg', 'trd_rvo', 'trd_pvo', 'trd_zad',
-                    missing, 'trd_ldf', 'trd_zdf', 'trd_tau',
-                    'trd_bfr', 'trd_bfri', 'trd_tot', 'trd_atf']
-
-        # plot
-        vmin, vmax = -1e-5, 1e-5
-        cmap=cmocean.cm.balance
-        for i, ax in enumerate(axs.flatten()):
-            ax.pcolor(ds[vec + var_list[i]], vmin=vmin, vmax=vmax, cmap=cmap)
-
-        # sum
-        #kesum = ds.ketrd_hpg + ds.ketrd_spg + ds.ketrd_keg + ds.ketrd_rvo +  \
-        #        ds.ketrd_pvo + ds.ketrd_zad + \
-        #        ds.ketrd_zdf + ds.ketrd_tau + \
-        #        ds.ketrd_atf + ds.ketrd_convP2K
-        #axs[3,2].pcolor(kesum, vmin=vmin, vmax=vmax, cmap=cmap)
-
-        # residule
-        #resid = kesum - trd_tot
-        #axs[3,3].pcolor(resid, vmin=vmin, vmax=vmax, cmap=cmap)
-
+        var_list = ['trd_hpg', 'trd_spg', 'trd_keg', 'trd_rvo',
+                    'trd_pvo', 'trd_zad', 'trd_zdf', 'trd_tot']
 
         # titles
-        axs[0,0].set_title('hyd p')
-        axs[0,1].set_title('surf p')
-        axs[0,2].set_title('surf p exp')
-        axs[0,3].set_title('surf p imp')
-        axs[1,0].set_title('ke grad')
-        axs[1,1].set_title('zeta')
-        axs[1,2].set_title('cori')
-        axs[1,3].set_title('vadv')
-        axs[2,0].set_title('udx')
-        axs[2,1].set_title('lat diff')
-        axs[2,2].set_title('z diff')
-        axs[2,3].set_title('tau')
-        axs[3,0].set_title('drag')
-        axs[3,1].set_title('drag impl')
-        axs[3,2].set_title('tot')
-        axs[3,3].set_title('filter')
-        #axs[3,4].set_title('residule')
+        titles = ['hyd\npressure grad',      'surf\npressure grad',
+                  'lateral\n advection (KE)', 'lateral\nadvection (zeta)',
+                  'Coriolis',               'vertical\nadvection',
+                  'vertical\ndiffusion',     'tendency' ]
 
+        # plot
+        vmin, vmax = -1e-4, 1e-4
+        cmap=cmocean.cm.balance
+        for i, ax in enumerate(axs.flatten()):
+            p = ax.pcolor(ds[vec + var_list[i]],
+                             vmin=vmin, vmax=vmax, cmap=cmap)
+            ax.text(0.5, 1.01, titles[i], va='bottom', ha='center',
+                    transform=ax.transAxes, fontsize=8)
+
+        for ax in axs[:-1,:].flatten():
+            ax.set_xticklabels([])
+        for ax in axs[:,1:].flatten():
+            ax.set_yticklabels([])
+        for ax in axs[-1,:].flatten():
+            ax.set_xlabel('x')
+        for ax in axs[:,0].flatten():
+            ax.set_ylabel('y')
+
+        pos0 = axs[0,-1].get_position()
+        pos1 = axs[-1,-1].get_position()
+        cbar_ax = fig.add_axes([0.79, pos1.y0, 0.02, pos0.y1 - pos1.y0])
+        cbar = fig.colorbar(p, cax=cbar_ax, orientation='vertical')
+        cbar.ax.text(9.0, 0.5, 'Momentum Tendency', fontsize=8,
+                     rotation=90, transform=cbar.ax.transAxes,
+                     va='center', ha='right')
+        ax.text(0.5, 0.99, vec + ' momentum', va='top', ha='center',
+                fontsize=10, transform=fig.transFigure)
+    
         plt.savefig(self.case + '_' + vec + '_mom_mld_budget.png')
 
     def plot_mom_residule_budget(self, vec='u'):
         # ini figure
-        fig, axs = plt.subplots(1, 3, figsize=(5.5,5.5))
+        fig, axs = plt.subplots(1, 3, figsize=(5.5,3))
+        plt.subplots_adjust(right=0.78)
 
         # load and slice
         ds = xr.open_dataset(self.preamble + 'mom' + vec + '.nc')
-        print (ds)
         ds = ds.sel({'depth' + vec: 30}, method='nearest') # depth
-        ds = ds.isel(time_counter=1)             # time
+        ds = ds.isel(time_counter=1)                       # time
 
             
         if vec == 'u':
-            mom_sum = ds.utrd_hpg + ds.utrd_spg + ds.utrd_keg + \
-                      ds.utrd_rvo + ds.utrd_pvo + ds.utrd_zad
+            mom_sum = ds.utrd_hpg + ds.utrd_spg + ds.utrd_keg - \
+                      ds.utrd_rvo + ds.utrd_pvo + ds.utrd_zad 
         if vec == 'v':
             mom_sum = ds.vtrd_hpg + ds.vtrd_spg + ds.vtrd_keg + \
                       ds.vtrd_rvo + ds.vtrd_pvo + ds.vtrd_zad
@@ -95,12 +87,30 @@ class plot_momentum(object):
         # plot
         vmin, vmax = -1e-5, 1e-5
         cmap=cmocean.cm.balance
-        axs[0].pcolor(ds[vec + 'trd_tot'], vmin=vmin, vmax=vmax, cmap=cmap)
+        #axs[0].pcolor(ts, vmin=vmin, vmax=vmax, cmap=cmap)
+        p = axs[0].pcolor(ds[vec + 'trd_tot'], vmin=vmin, vmax=vmax, cmap=cmap)
         axs[1].pcolor(mom_sum, vmin=vmin, vmax=vmax, cmap=cmap)
         axs[2].pcolor(mom_sum-ds[vec+'trd_tot'], vmin=vmin, vmax=vmax, cmap=cmap)
-        axs[0].set_title('tot')
-        axs[1].set_title('sum')
-        axs[2].set_title('residule')
+        #axs[2].pcolor(mom_sum-ts, vmin=vmin, vmax=vmax, cmap=cmap)
+        axs[0].set_title('tendency')
+        axs[1].set_title('sum of RHS')
+        axs[2].set_title('residual')
+
+        pos = axs[-1].get_position()
+        cbar_ax = fig.add_axes([0.79, pos.y0, 0.02, pos.y1 - pos.y0])
+        cbar = fig.colorbar(p, cax=cbar_ax, orientation='vertical')
+        cbar.ax.text(9.0, 0.5, 'Momentum Tendency', fontsize=8,
+                     rotation=90, transform=cbar.ax.transAxes,
+                     va='center', ha='right')
+
+        for ax in axs[1:]:
+            ax.set_yticklabels([])
+        for ax in axs:
+            ax.set_xlabel('x')
+        axs[0].set_ylabel('y')
+        for ax in axs:
+            ax.set_aspect('equal')
+
         plt.savefig(self.case + '_' + vec + '_mom_mld_budget_resid.png')
 
     
