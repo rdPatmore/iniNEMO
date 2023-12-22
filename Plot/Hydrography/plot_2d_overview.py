a9920c8af62eae08d7b2f6b27316077835fd9af4
rdPatmore <r.d.patmore@reading.ac.uk>
2023-12-22 15:52:59 +0000

    plotting additions

--- Plot/Hydrography/plot_2d_overview.py
+++ Plot/Hydrography/plot_2d_overview.py
@@ -1,9 +1,10 @@
 import xarray as xr
 import matplotlib.pyplot as plt
+import matplotlib
 import config
 import cmocean
 
-
+matplotlib.rcParams.update({'font.size': 8})
 
 # load data 
 file_id = '/SOCHIC_PATCH_3h_20121209_20130331_'
@@ -14,7 +15,7 @@ bg_mod2 = xr.open_dataset(preamble + 'bg_mod2.nc', **kwargs).bg_mod2**0.5
 ice = xr.open_dataset(preamble + 'icemod.nc', **kwargs).siconc
 
 # select time
-tslice = '2013-01-08 12:00:00'
+tslice = '2012-12-24 12:00:00'
 
 depth = 10
 sel_dict = dict(time_counter=tslice, deptht=depth)
@@ -29,13 +30,18 @@ ice = ice.isel(bounds)
 bg_mod2 = bg_mod2.isel(bounds)
 
 # plot
-fig, axs = plt.subplots(1,3)
-plt.subplots_adjust(bottom=0.3)
-p0 = axs[0].pcolor(mld, vmin=0, vmax=120)
-p1 = axs[1].pcolor(bg_mod2, vmin=0, vmax=2e-7)
-p2 = axs[2].pcolor(ice, cmap=cmocean.cm.ice, vmin=0, vmax=1)
+fig, axs = plt.subplots(1,3, figsize=(6.5,3.0))
+plt.subplots_adjust(bottom=0.3, top=0.99, right=0.98, left=0.1, hspace=0.05)
+p0 = axs[0].pcolor(mld.nav_lon, mld.nav_lat, mld, vmin=0, vmax=120,
+                   shading='nearest')
+p1 = axs[1].pcolor(bg_mod2.nav_lon, bg_mod2.nav_lat, bg_mod2, vmin=0, vmax=2e-7,
+                   shading='nearest')
+p2 = axs[2].pcolor(ice.nav_lon, ice.nav_lat, ice, cmap=cmocean.cm.ice,
+                   vmin=0, vmax=1, shading='nearest')
 p = [p0,p1,p2]
-txt = ['mld', 'bg', 'sea ice']
+txt = ['Mixed Layer Depth (m)',
+        r'$|\nabla b|$ (s$^{-2}$)',
+        'Sea Ice Concentration (-)']
 
 for i, ax in enumerate(axs):
     # aspect ratio
@@ -45,9 +51,16 @@ for i, ax in enumerate(axs):
     pos = ax.get_position()
     cbar_ax = fig.add_axes([pos.x0, 0.15, pos.x1 - pos.x0, 0.02])
     cbar = fig.colorbar(p[i], cax=cbar_ax, orientation='horizontal')
-    cbar.ax.text(0.5, -3.5, txt[i], fontsize=8,
+    cbar.ax.text(0.5, -5.0, txt[i], fontsize=8,
                  rotation=0, transform=cbar.ax.transAxes,
                  va='center', ha='center')
+
+# set axis labels
+axs[0].set_ylabel(r'Latitude ($^{\circ}$N)')
+for ax in axs:
+    ax.set_xlabel(r'Longitude ($^{\circ}$E)')
+for ax in axs[1:]:
+    ax.set_yticklabels([])
 
 # save
 plt.savefig('mld_and_bg_' + tslice.split(' ')[0] +  '.png', dpi=300)
