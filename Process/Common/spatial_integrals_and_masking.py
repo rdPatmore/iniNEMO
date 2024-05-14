f7877e562eba4fdb03c8a14c7d224356a111d6b2
rdPatmore <r.d.patmore@reading.ac.uk>
2024-05-14 15:12:09 +0100

    first draft of common functions

--- Process/Common/spatial_integrals_and_masking.py
+++ Process/Common/spatial_integrals_and_masking.py
@@ -1,8 +1,5 @@
 import xarray as xr
 import config
-import matplotlib.pyplot as plt
-import numpy as np
-import dask
 from dask.diagnostics import ProgressBar
 
 class integrals_and_masks(object):
@@ -10,8 +7,9 @@ class integrals_and_masks(object):
     Integrated variables in the dimensions of choice
     '''
 
-    def __init__(self, case, file_id, var):
+    def __init__(self, case, file_id, var, var_str):
         self.var = var
+        self.var_str = var_str
         self.file_id = file_id
         self.preamble = config.data_path() + case +  self.file_id
         self.path = config.data_path() + case + '/'
@@ -23,7 +21,16 @@ class integrals_and_masks(object):
         kwargs = {'chunks': dict(time=1)} 
         ds = xr.open_dataset(self.preamble + 'grid_T.nc', **kwargs)
 
-        self.var_ml =  var.where(ds.e3t < ds.mldr10_3, drop=False)
+        # convert cell thickness to depths
+        deps = ds_mean.e3t.cumsum('deptht')
+
+        self.var_ml =  var.where(deps < ds.mldr10_3, drop=False)
+
+    def cut_edges(self, rim=slice(10,-10), var):
+        ''' cut forcing rim '''
+
+        return var.isel(x=rim, y=rim)
+        
 
     def domain_mean_ml_KE_ice_oce_zones(self, threshold=0.2):
         '''
@@ -40,21 +47,12 @@ class integrals_and_masks(object):
 
         # load ice concentration
         icemsk = xr.open_dataset(self.preamble + 'icemod.nc',
-                            chunks={'time_counter':10}, decode_cf=True).siconc
-
-        # mask e3t by mean mixed layer depth
-        e3t_mean_ml = self.mask_by_ml(self.e3t_mean)
+                            chunks={'time_counter':10}).siconc
 
         # cut edges
-        cut=slice(10,-10)
-        self.tke_mld = self.tke_mld.isel(x=cut,y=cut)
-        cfg = cfg.isel(x=cut,y=cut)
-        icemsk = icemsk.isel(x=cut,y=cut)
-        e3t_mean_ml = e3t_mean_ml.isel(x=cut,y=cut)
-        self.e3t_mean = self.e3t_mean.isel(x=cut,y=cut)
-
-        # mean ice conc over time series
-        icemsk_mean = icemsk.mean('time_counter')
+        self.var_ml = self.cut_edges(self.var_ml)
+        cfg = self.cut_edges(cfg)
+        icemsk = self.cut_edges(icemsk)
 
         # get masks
         miz_msk = (icemsk_mean > threshold) & (icemsk_mean < (1 - threshold))
@@ -62,29 +60,32 @@ class integrals_and_masks(object):
         oce_msk = icemsk_mean < threshold
 
         # mask by ice concentration
-        tke_mld_miz = self.tke_mld.where(miz_msk)
-        tke_mld_ice = self.tke_mld.where(ice_msk)
-        tke_mld_oce = self.tke_mld.where(oce_msk)
+        var_ml_miz = self.var_ml.where(miz_msk)
+        var_ml_ice = self.var_ml.where(ice_msk)
+        var_ml_oce = self.var_ml.where(oce_msk)
+
+        # define mean dims
+        dims = ['x','y','deptht']
 
         # find volume of each partition
         area = cfg.e2t * cfg.e1t
         t_vol = area * e3t_mean_ml
-        t_vol_miz = t_vol.where(miz_msk).sum()
-        t_vol_ice = t_vol.where(ice_msk).sum()
-        t_vol_oce = t_vol.where(oce_msk).sum()
+        t_vol_miz = t_vol.where(miz_msk).sum(dim=dims)
+        t_vol_ice = t_vol.where(ice_msk).sum(dim=dims)
+        t_vol_oce = t_vol.where(oce_msk).sum(dim=dims)
 
         # calculate volume weighted mean
         t_volume = self.e3t_mean * area
-        tke_integ_miz = (tke_mld_miz * t_volume).sum() / t_vol_miz
-        tke_integ_ice = (tke_mld_ice * t_volume).sum() / t_vol_ice
-        tke_integ_oce = (tke_mld_oce * t_volume).sum() / t_vol_oce
+        var_integ_miz = (var_ml_miz * t_volume).sum(dim=dims) / t_vol_miz
+        var_integ_ice = (var_ml_ice * t_volume).sum(dim=dims) / t_vol_ice
+        var_integ_oce = (var_ml_oce * t_volume).sum(dim=dims) / t_vol_oce
 
         # save
         with ProgressBar():
-            fn = self.preamble + 'TKE_budget_domain_integ_{}.nc'
-            tke_integ_miz.to_netcdf(fn.format('miz'))
-            tke_integ_ice.to_netcdf(fn.format('ice'))
-            tke_integ_oce.to_netcdf(fn.format('oce'))
+            fn = self.preamble + self.var_str + '_domain_integ_{}.nc'
+            var_integ_miz.to_netcdf(fn.format('miz'))
+            var_integ_ice.to_netcdf(fn.format('ice'))
+            var_integ_oce.to_netcdf(fn.format('oce'))
 
     def horizontal_mean_ml_KE_ice_oce_zones(self, threshold=0.2):
         '''
@@ -96,20 +97,17 @@ class integrals_and_masks(object):
         '''
 
         # get data and domain_cfg
+        self.mask_by_ml()
         cfg = xr.open_dataset(self.path + 'domain_cfg.nc', chunks=-1)
 
         # load ice concentration
         icemsk = xr.open_dataset(self.preamble + 'icemod.nc',
-                            chunks={'time_counter':10}, decode_cf=True).siconc
+                            chunks={'time_counter':10}).siconc
 
         # cut edges
-        cut=slice(10,-10)
-        self.tke_mld = self.tke_mld.isel(x=cut,y=cut)
-        cfg = cfg.isel(x=cut,y=cut)
-        icemsk = icemsk.isel(x=cut,y=cut)
-
-        # mean ice conc over time series
-        icemsk_mean = icemsk.mean('time_counter')
+        self.var_ml = self.cut_edges(self.var_ml)
+        cfg = self.cut_edges(cfg)
+        icemsk = self.cut_edges(icemsk)
 
         # get masks
         miz_msk = (icemsk_mean > threshold) & (icemsk_mean < (1 - threshold))
@@ -117,9 +115,9 @@ class integrals_and_masks(object):
         oce_msk = icemsk_mean < threshold
 
         # mask by ice concentration
-        tke_mld_miz = self.tke_mld.where(miz_msk)
-        tke_mld_ice = self.tke_mld.where(ice_msk)
-        tke_mld_oce = self.tke_mld.where(oce_msk)
+        var_ml_miz = self.var_ml.where(miz_msk)
+        var_ml_ice = self.var_ml.where(ice_msk)
+        var_ml_oce = self.var_ml.where(oce_msk)
 
         # find area of each partition
         area = cfg.e2t * cfg.e1t
@@ -128,14 +126,14 @@ class integrals_and_masks(object):
         area_oce = area.where(oce_msk).sum()
 
         # calculate lateral weighted mean
-        tke_integ_miz = (tke_mld_miz * area).sum(dim=['x','y']) / area_miz
-        tke_integ_ice = (tke_mld_ice * area).sum(dim=['x','y']) / area_ice
-        tke_integ_oce = (tke_mld_oce * area).sum(dim=['x','y']) / area_oce
+        var_integ_miz = (var_ml_miz * area).sum(dim=['x','y']) / area_miz
+        var_integ_ice = (var_ml_ice * area).sum(dim=['x','y']) / area_ice
+        var_integ_oce = (var_ml_oce * area).sum(dim=['x','y']) / area_oce
 
         # save
         with ProgressBar():
-            fn = self.preamble + 'TKE_budget_horizontal_integ_{}.nc'
-            tke_integ_miz.to_netcdf(fn.format('miz'))
-            tke_integ_ice.to_netcdf(fn.format('ice'))
-            tke_integ_oce.to_netcdf(fn.format('oce'))
+            fn = self.preamble + self.var_str + '_horizontal_integ_{}.nc'
+            var_integ_miz.to_netcdf(fn.format('miz'))
+            var_integ_ice.to_netcdf(fn.format('ice'))
+            var_integ_oce.to_netcdf(fn.format('oce'))
 
