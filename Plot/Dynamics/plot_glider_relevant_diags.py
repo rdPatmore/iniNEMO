f1007145605c7140f5f6cf8a1a5f28b8820137ad
rdPatmore <ryapat@noc.ac.uk>
2024-06-28 13:41:54 +0100

    reformat and add ml_mid

--- Plot/Dynamics/plot_glider_relevant_diags.py
+++ Plot/Dynamics/plot_glider_relevant_diags.py
@@ -5,7 +5,7 @@ import matplotlib
 
 matplotlib.rcParams.update({"font.size": 8})
 
-def plot_time_series(case):
+def plot_time_series(case, ml_mid=False):
     """ 
     Plot model time series of:
         - N2
@@ -24,68 +24,38 @@ def plot_time_series(case):
     # data source
     path = config.data_path() + case + "/TimeSeries/"
 
-    # render Temperature
-    T = xr.open_dataset(path + "votemper_domain_integ.nc")
-    axs[0].plot(T.time_counter, T.votemper_oce_weighted_mean, label="Oce")
-    axs[0].plot(T.time_counter, T.votemper_miz_weighted_mean, label="MIZ")
-    axs[0].plot(T.time_counter, T.votemper_ice_weighted_mean, label="ICE")
-    axs[0].set_ylabel("Temperature")
+    def render(ax, var, integ_type, title):
 
-    # render Salinity
-    S = xr.open_dataset(path + "vosaline_domain_integ.nc")
-    axs[1].plot(S.time_counter, S.vosaline_oce_weighted_mean)
-    axs[1].plot(S.time_counter, S.vosaline_miz_weighted_mean)
-    axs[1].plot(S.time_counter, S.vosaline_ice_weighted_mean)
-    axs[1].set_ylabel("Salinity")
-    
-    # render MLD
-    mld = xr.open_dataset(path + "mld_horizontal_integ.nc")
-    axs[2].plot(mld.time_counter, mld.mld_oce_weighted_mean)
-    axs[2].plot(mld.time_counter, mld.mld_miz_weighted_mean)
-    axs[2].plot(mld.time_counter, mld.mld_ice_weighted_mean)
-    axs[2].set_ylabel("MLD")
-    
-    # render N2
-    N2 = xr.open_dataset(path + "N2_mld_horizontal_integ.nc")
-    axs[3].plot(N2.time_counter, N2.N2_mld_oce_weighted_mean)
-    axs[3].plot(N2.time_counter, N2.N2_mld_miz_weighted_mean)
-    axs[3].plot(N2.time_counter, N2.N2_mld_ice_weighted_mean)
-    axs[3].set_ylabel("N$^2$")
+        ds = xr.open_dataset(path + var + "_" + integ_type + ".nc")
+        ax.plot(ds.time_counter, ds[var + "_oce_weighted_mean", label="Oce")
+        ax.plot(ds.time_counter, ds[var + "_miz_weighted_mean", label="MIZ")
+        ax.plot(ds.time_counter, ds[var + "_ice_weighted_mean", label="ICE")
+        ax.set_ylabel(title)
 
-    # render buoyancy gradinets
-    bg = xr.open_dataset(path + "bg_mod2_domain_integ.nc")
-    axs[4].plot(bg.time_counter, bg.bg_mod2_oce_weighted_mean)
-    axs[4].plot(bg.time_counter, bg.bg_mod2_miz_weighted_mean)
-    axs[4].plot(bg.time_counter, bg.bg_mod2_ice_weighted_mean)
-    axs[4].set_ylabel(r"$|\mathbf{\nabla}b|$")
+    if ml_mid:
+        integ_str = "ml_mid_horizontal_integ"
+        render(axs[0], "votemper", integ_str, "Temperature")
+        render(axs[1], "vosaline", integ_str, "Salinity")
+        render(axs[3], "bn2", integ_str, r"N$^2$")
+        render(axs[4], "bg_mod2", integ_str, r"$|\mathbf{\nabla}b|$")
+    else:
+        render(axs[0], "votemper", "domain_integ", "Temperature")
+        render(axs[1], "vosaline", "domain_integ", "Salinity")
+        render(axs[3], "N2", "horizontal_integ", r"N$^2$")
+        render(axs[4], "bg_mod2", "domain_integ", r"$|\mathbf{\nabla}b|$")
 
-    # render wind speed gradinets
-    wind = xr.open_dataset(path + "windsp_horizontal_integ.nc")
-    axs[5].plot(wind.time_counter, wind.windsp_oce_weighted_mean)
-    axs[5].plot(wind.time_counter, wind.windsp_miz_weighted_mean)
-    axs[5].plot(wind.time_counter, wind.windsp_ice_weighted_mean)
-    axs[5].set_ylabel(r"$U_{10}$")
-
-    # render buoyancy gradinets
-    wfo = xr.open_dataset(path + "wfo_horizontal_integ.nc")
-    axs[6].plot(wfo.time_counter, wfo.wfo_oce_weighted_mean)
-    axs[6].plot(wfo.time_counter, wfo.wfo_miz_weighted_mean)
-    axs[6].plot(wfo.time_counter, wfo.wfo_ice_weighted_mean)
-    axs[6].set_ylabel(r"$Q_{fw}$")
-
-    # render wind stress 
-    taum = xr.open_dataset(path + "taum_horizontal_integ.nc")
-    axs[7].plot(taum.time_counter, taum.taum_oce_weighted_mean)
-    axs[7].plot(taum.time_counter, taum.taum_miz_weighted_mean)
-    axs[7].plot(taum.time_counter, taum.taum_ice_weighted_mean)
-    axs[7].set_ylabel(r"$|\mathbf{\tau}_s|$")
+    render(axs[2], "mld", "horizontal_integ", "MLD")
+    render(axs[5], "windsp", "horizontal_integ", r"$U_{10}$")
+    render(axs[6], "wfo", "horizontal_integ", r"$Q_{fw}$")
+    render(axs[7], "taum", "horizontal_integ", r"$|\mathbf{\tau}_s|$")
 
     for ax in axs[:7]:
         ax.set_xticks([])
 
     axs[0].legend()
 
-    plt.savefig("glider_relevant_diags.pdf")
+    if ml_mid: append = "_ml_mid" else append: = ""
+    plt.savefig("glider_relevant_diags{}.pdf".format(append))
 
 if __name__ == "__main__":
 
